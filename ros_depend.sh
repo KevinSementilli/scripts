03c1910fbcba82189eded49b4c77d31a3afa8be7
KevinSementilli <ksementilli@hotmail.ca>
2025-04-20 16:35:15 -0400

    Update dependencies and fixed ROS env setup

--- ros_depend.sh
+++ ros_depend.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-echo "installing gazebo"
+echo "\n\n Installing gazebo \n"
 sudo apt install ros-humble-gazebo-ros-pkgs
 
-echol "installing ros2_control"
-sudo apt install ros-humble-ros2-control
+echo -e "\n\n Installing ros2_control \n"
+sudo apt install ros-humble-ros2-control ros-humble-ros2-controllers
 
-echo " installing rqt"
+echo -e "\n\n Installing rqt \n"
 sudo apt install ros-humble-rqt
+
+echo -e "\n\n Installing serial library for uart protocol \n"
+sudo apt install libserial-dev
+
+echo -e "\n\n Installing Joy Node \n"
+sudo apt install ros-humble-joy
+
+echo -e "\n\n ALL DONE!! \n"
--- ros_setup.sh
+++ ros_setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "Updating dependencies and getting the latest updates"
+echo -e "\n\n Updating dependencies and getting the latest updates\n"
 sudo apt update
 sudo apt upgrade -y  # Automatically agree to upgrade
 
 # Setup sources for ROS 2 Humble
-echo "Installing necessary dependencies"
+echo -e "\n\n Installing necessary dependencies\n"
 sudo apt install -y software-properties-common curl gnupg lsb-release
 
-echo "Adding ROS 2 repository"
+echo -e "\n\n Adding ROS 2 repository\n"
 sudo add-apt-repository universe -y
 sudo apt update
 
@@ -17,17 +17,15 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 # Install ROS 2 Humble
-echo "Installing ROS 2 Humble"
+echo -e "\n\n Installing ROS 2 Humble\n"
 sudo apt update
 sudo apt install -y ros-humble-desktop
 
-# Source ROS 2 setup and update bashrc
-echo "Setting up ROS 2 environment"
-source /opt/ros/humble/setup.bash
-echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
-
 # Install colcon for building ROS 2 packages
-echo "Installing colcon"
+echo -e "\n\n Installing colcon\n"
 sudo apt install -y python3-colcon-common-extensions
 
-echo "ROS 2 Humble setup complete!"
+echo -e "\n\n ROS 2 Humble setup complete!"
+
+# Source ROS 2 setup and update bashrc
+echo -e "\n\n Run the following command in home directory \n\n source /opt/ros/humble/setup.bash \n echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
